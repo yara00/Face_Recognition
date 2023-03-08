94e6b5eb16f42144e866bb07930c649b88498076
yarahossamboo <yarahossam222@gmail.com>
2023-03-08 19:11:45 +0200

    dataset uploaded

--- Dataset.py
+++ Dataset.py
@@ -6,22 +6,24 @@ from PIL import Image
 class Dataset:
     def __init__(self):
         self.data_matrix = np.zeros(shape=(400, 10304))
-        self.training_set = np.zeros(shape=(200, 10304))
-        self.test_set = np.zeros(shape=(200, 10304))
         self.labels = []
 
     def generate_matrix(self):
         for sample_ctr in range(1, 41):
             for img_ctr in range(1, 11):
-                img = Image.open('/content/drive/MyDrive/dataset/s' + str(sample_ctr) + '/' + str(img_ctr) + '.pgm')
+                img = Image.open('dataset/s' + str(sample_ctr) + '/' + str(img_ctr) + '.pgm')
                 image_vector = np.asarray(img).flatten()
                 self.data_matrix[((sample_ctr - 1) * 10) + (img_ctr - 1)] = image_vector
                 self.labels.append(sample_ctr)
         return self.data_matrix, self.labels
 
     def split_matrix(self):
-        self.training_set = self.data_matrix[::2, :]  # odd rows for training
-        self.training_labels = self.labels[::2]
-        self.test_set = self.data_matrix[1::2, :]  # even rows for testing
-        self.test_labels = self.labels[1::2]
-        return self.training_set, self.training_labels, self.test_set, self.test_labels
+        self.generate_matrix()
+        training_set = self.data_matrix[::2, :]  # odd rows for training
+        training_labels = self.labels[::2]
+        test_set = self.data_matrix[1::2, :]  # even rows for testing
+        test_labels = self.labels[1::2]
+        return training_set, training_labels, test_set, test_labels
+
+
+
