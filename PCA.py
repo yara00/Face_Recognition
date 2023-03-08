8756cbca462557d527760e2f5dc54521e08bb75e
MariamOsman21 <76537563+MariamOsman21@users.noreply.github.com>
2023-03-09 01:12:07 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -6,11 +6,7 @@ class PCA:
         pass
 
     def projection_matrix(self,D, alpha):
-        D_transpose = np.transpose(D)
-        #mean_vec = np.mean(D, axis=0)
-        #Z = D - mean_vec
         cov_matrix = np.cov(np.transpose(D), bias = True)
-        #print(cov_matrix)
         eigen_values, eigen_vectors = self.__eigen_sorted(cov_matrix)
         return self.__calculate_projection_matrix(eigen_values, eigen_vectors, alpha)
 
@@ -27,15 +23,18 @@ class PCA:
         eigen_i = 0
         idx = -1;
         for i in eigen_values:
-            print(i)
+            print(i , ' eigh')
 
         for i in range(0, len(eigen_values)):
             eigen_i += eigen_values[i]
             #print(eigen_i / eigen_sum)
             if (eigen_i / eigen_sum) >= alpha:
-                idx = i;
-                break;
+                idx = i
+                break
         return np.transpose(eigen_vectors[: idx + 1])
+
+    def projected_data(self, P, training_set, test_set):
+        return np.matmul(P, training_set), np.matmul(P, test_set)
 
 
 def write_mat(mat, alpha):
@@ -44,18 +43,13 @@ def write_mat(mat, alpha):
 
 dataset = Dataset()
 dataset.generate_matrix()
-training_set, test_set , training_labels, test_labels = dataset.split_matrix()
+training_set, test_set, training_labels, test_labels = dataset.split_matrix()
 
 pca = PCA()
-
-
 alpha = [0.8,0.85,0.9,0.95]
 
-'''
 for i in range(0, len(alpha)):
     P = pca.projection_matrix(training_set, alpha[i])
-    write_mat(P, alpha[i])
-'''
-P = np.loadtxt('PCA alpha = 0.95.txt')
-print(np.matmul(training_set, P))
+    training_set, test_set = pca.projected_data(P, training_set, test_set)
+
 
